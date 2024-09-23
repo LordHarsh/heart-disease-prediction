fbff70e3c27fe74e47d46165dc0ee54b99ed5ef1
Harsh Banka <harshbanka321@gmail.com>
2024-09-23 20:23:12 +0530

    fix: import status

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
